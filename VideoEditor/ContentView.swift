50c95c5d50deeed1be55c05fe84080bd52483eb4
张 <wz2647@columbia.edu>
2024-07-18 15:08:56 -0400

    Initial Commit

---
+++ VideoEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoEditor
+//
+//  Created by 张 on 2024/7/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoEditor/VideoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEditorApp.swift
+//  VideoEditor
+//
+//  Created by 张 on 2024/7/18.
+//
+
+import SwiftUI
+
+@main
+struct VideoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
